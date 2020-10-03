927855ff088dfad1dae09d346f33bace435b11fa
somkuwaryash <63736665+somkuwaryash@users.noreply.github.com>
2020-10-03 18:33:19 +0530

    Create Calculator - Copy.py

---
+++ Calculator - Copy.py
@@ -0,0 +1,15 @@
+#This code is made by Yash Somkuwar
+
+def Fibonacci(n):
+	if n<=0:
+		print("Incorrect input")
+	
+	elif n==1:
+		return 0
+	
+	elif n==2:
+		return 1
+	else:
+		return Fibonacci(n-1)+Fibonacci(n-2)
+
+

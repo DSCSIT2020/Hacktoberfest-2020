27f83017a5ac8eac5acc1638f1749db62aed4388
Double 77 <danielread55@hotmail.co.uk>
2020-10-03 15:26:58 +0100

    Heap Sort Algorithm
    Sorts an array of numbers using the heap sorting algorithm

---
+++ Sorting Algorithms/HeapSort.py
@@ -0,0 +1,31 @@
+def heap(arr, n, i): 
+    largest = i  
+    left = 2 * i + 1     
+    right = 2 * i + 2    
+    if left < n and arr[i] < arr[left]: 
+        largest = left
+  
+    if right < n and arr[largest] < arr[right]: 
+        largest = right
+
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] 
+  
+        heap(arr, n, largest) 
+
+def HeapSort(arr): 
+    n = len(arr) 
+ 
+    for i in range(n // 2 - 1, -1, -1): 
+        heap(arr, n, i) 
+  
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]  
+        heap(arr, i, 0)   
+
+array = [ 12, 11, 13, 5, 6, 7] 
+HeapSort(array) 
+n = len(array) 
+print ("The Sorted array is") 
+for i in range(n): 
+    print ("%d" %array[i]), 
